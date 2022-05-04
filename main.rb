317e329a14562fd1875c77094fdb6c810f15252b
Hunter Adams <hunteradams10@gmail.com>
2022-05-04 10:51:21 +1000

    created class variable and made notes on how to access it from related file

--- main.rb
+++ main.rb
@@ -1,10 +1,25 @@
 require_relative 'bicycle'
+
+def best_vehicle(planned_terrain, distance, vehicles) 
+
+end
+
+p best_vehicle(:road, 50)
+
+# You would call the class variable by giving the Class, then its method
+# ie. Vehicles.vehicle_collection
+
+
 # Display
+
+victor = Bicycle.new(10, [:road, :dirt])
+daisy = Motorbike.new(20, [:dirt])
+
 # bob = Vehicle.new(20, [:road], 2, "brum")
 # bob.refuel(35)
 
 # p bob.fuel_kms
 
-victor = Bicycle.new(10, [:road, :dirt])
-p victor
-p victor.noise
+# victor = Bicycle.new(10, [:road, :dirt])
+# p victor
+# p victor.noise
--- vehicle.rb
+++ vehicle.rb
@@ -1,15 +1,27 @@
 class Vehicle
     attr_reader :speed, :terrain, :fuel_kms
+
+    @@vehicle_collection = []
+
+
     def initialize(speed, terrain, max_passengers, noise)
         @speed = speed
         @terrain = terrain 
         @max_passengers = max_passengers
         @noise = noise 
         @fuel_kms = 0
+        # this is to append to the array @@vehicle_collection above ^^
+        @@vehicle_collection << self
 end
 
     def refuel(fuel_amount)
         @fuel_kms += fuel_amount
+    end
+
+    # You need this method in order to access the array above. Much like how you need a method
+    # or attr_reader/writer/accessor to access other instance variables in the Vehicle class.
+    def self.vehicle_collection
+        @@vehicle.collection 
     end
 
     #WE COULD DO
